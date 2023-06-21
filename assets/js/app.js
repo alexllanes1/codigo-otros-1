3f078cec40bbd023cfd11bd04ea73cffb3d3bc7c
alexllanes1 <alexllanes026@gmail.com>
2023-06-21 08:29:46 -0600

    Se arreglo y colocaron comentarios al codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,25 @@
-const baseEndpoint = 'https://api.github.com';
+//Declaración de constantes
+const baseEndpoint = 'https://api.github.com'; //Link de la API de github
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+
+//DOM
+const $n = document.querySelector('.name'); //Falta de . para seleccionar la clase name
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+  const response = await fetch(`${usersEndpoint}/${username}`); 
+  //Error presente, faltaba el async al inicio de la función por lo que el await no podía ser usado.
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+
+  //No se usaban ` si no '
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
+//Función que permite mostrar un mensaje de error cuando este se presente, el parametro err nos mostrará que tipo de error es el que se nos ha presentado.
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
